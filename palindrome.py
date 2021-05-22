8c2fa1c168e53664fdcac01564c110860f0ac485
Reu. Ekow Amissah <75743058+Reu-Amissah@users.noreply.github.com>
2021-05-22 11:52:10 +0000

    update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,26 @@
 #python palindrome
+
+import math
+
+#palindrome-check function with userInput parameter
+def palindrome_check(word):
+    isPalindrome = bool
+    numLimit = math.floor(len(word) / 2)
+    for i in range(numLimit):
+      
+        # compare letters
+        if word[i] == word[len(word) - 1 - i]:
+            isPalindrome = True
+            continue
+        else:
+            isPalindrome = False
+            break
+
+    if isPalindrome:
+        print("This is a palindrome")
+    else:
+        print("This is not a palindrome")
+
+#acquire user - input
+userInput= input("Enter your word: ")
+palindrome_check(userInput)
